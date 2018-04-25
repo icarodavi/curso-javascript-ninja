1efa0f583eb02bf1ff54c59a2e709385006e1d1f
icarodavi <icarodavi@gmail.com>
2018-04-25 20:04:15 -0300

    Challenge 11

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -1,4 +1,4 @@
-// Envolva todo o código desse arquivo em uma IIFE (incluindo esse comentário).
+(function() {// Envolva todo o código desse arquivo em uma IIFE (incluindo esse comentário).
 
 /*
 Crie uma variável chamada `once`, que recebe `false` como seu valor.
@@ -7,14 +7,22 @@ loop, mostre no console a mensagem:
 'Entrou ao menos uma vez!'
 Qual loop você deve usar para que essa mensagem seja mostrada no console?
 */
-// ?
+var once = false;
+do {
+  console.log('Entrou ao menos uma vez!');
+} while ( once );
 
 /*
 Crie um objeto chamado `person`, que receba as seguintes propriedades:
 - 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
 para o nome, idade, peso e data de nascimento dessa pessoa.
 */
-// ?
+var person = {
+  name: 'Icaro',
+  age: 31,
+  weight: 86,
+  birthday: '1st Oct 1986',
+}
 
 /*
 Use um loop para percorrer o objeto criado acima, mostrando no console
@@ -25,8 +33,12 @@ esse objeto tem.
 Após o loop, mostre a frase:
 'The person has [COUNTER] properties'
 */
-// ?
-
+counter = 0;
+for (item in person) {
+  counter++;
+  console.log('The '+item+' of person is '+person[item]);
+}
+console.log('The person has '+counter+' properties');
 /*
 Crie uma função chamada `moreThan`, que vai verificar se a pessoa (objeto
 criado acima) é mais velha que a idade passada por parâmetro.
@@ -34,8 +46,14 @@ Se verdadeiro, retornar `true`. Senão, retornar false.
 Após a função, mostrar a mensagem no console:
 'The person has more than 25 years old? [TRUE/FALSE]'
 */
-// ?
+function moreThan(x) {
+  if(x < person.age) {
+    return true;
+  }
+    return false;
+}
 
+console.log('The person has more than 25 years old? '+moreThan(25));
 /*
 Faça um loop de 0 a 20, que adicione cada número como um item de um
 array chamado `numbers`. Se o contador for maior que 10, saia do loop.
@@ -52,3 +70,4 @@ Mostrar no console os números do array.
 */
 console.log( 'Pares de 0 a 20:' );
 // ?
+})();
