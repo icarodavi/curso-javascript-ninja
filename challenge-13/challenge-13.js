3cd7a9500307466a10ed770cc6f48a97da28ca1d
icarodavi <icarodavi@gmail.com>
2018-04-28 14:36:13 -0300

    Challenge 13

--- challenge-13/challenge-13.js
+++ challenge-13/challenge-13.js
@@ -102,7 +102,7 @@
   */
  var newBrasil = [];
   brasil.forEach(function(item, id) {
-    newBrasil.push({id: id, item: item});
+    newBrasil.push({id: id, estado: item});
   });
 
   /*
@@ -144,7 +144,7 @@
   Atribua o novo array a uma variável chamada `map`.
   */
   var map = newBrasil.map(function(item, id, arr) {
-    return newBrasil
+    return {id: item.id + 1, estado: item.estado + ' pertence ao Brasil.', };
   });
 
   /*
@@ -157,11 +157,13 @@
   Filtre o array criado acima, retornando somente os estados que tiverem
   ID par. Atribua o valor à uma variável chamada `filter`.
   */
-  // ?
+  var filter = map.filter(function(item) {
+    return item.id % 2 === 0;
+  });
 
   /*
   Mostre o array filtrado acima no console.
   */
   console.log( '\nEstados com ID par:' );
-  // ?
+  console.log(filter);
 })();
